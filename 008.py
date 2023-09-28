ec03d0301ace95822d7daaad589476495e5dce2b
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2023-09-28 03:24:44 -0300

    008.py

---
+++ 008.py
@@ -0,0 +1,8 @@
+A = float(input("Uma distancia em metros: "))
+print(f"A medida de {A} corresponde a:")
+print(f"{A * 0.001}Km")
+print(f"{A * 0.01}Hm")
+print(f"{A * 0.1:.1f}dam")
+print(f"{A * 10:.0f}dm")
+print(f"{A * 100:.0f}cm")
+print(f"{A * 1000:.0f}mm")
