cc910f6554424da5199710bcc6f7d87181b88495
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2023-10-10 10:53:10 -0300

    Update 020.py

--- 020.py
+++ 020.py
@@ -6,6 +6,6 @@ a3 = input("Terceiro aluno: ")
 a4 = input("Quarto aluno: ")
 
 lista = [a1, a2, a3, a4]
-a5 = random.sample(lista, 4)
+random.shuffle(lista)
 
-print(f"A ordem de apresentação será: {a5}")
+print(f"A ordem de apresentação será: {lista}")
