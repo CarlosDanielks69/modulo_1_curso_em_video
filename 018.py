245654a2c902cdb31899404a67c1398a2f486f1a
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2023-10-10 08:35:00 -0300

    Create 018.py

---
+++ 018.py
@@ -0,0 +1,12 @@
+import math
+
+
+angulo_radianos = math.radians(30)
+
+seno = math.sin(angulo_radianos)
+cosseno = math.cos(angulo_radianos)
+tangente = math.tan(angulo_radianos)
+
+print("Seno:", seno)
+print("Cosseno:", cosseno)
+print("Tangente:", tangente)
