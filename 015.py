17d250b4797a091f6edf7500881becda171a686b
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-04-26 17:11:36 -0300

    Cálculo de custo de aluguel de carros

--- 015.py
+++ 015.py
@@ -1,4 +1,4 @@
-dias = float(input("Quantos dias alugados? "))
-km = float(input("Quantos Km rodados? "))
-total = dias * 60 + km * 0.15
-print(f"Valor total a pagar: R${total:.2f}")
+d = float(input('Alugado por quantos dias? '))
+km = float(input('Quantos Km foram rodados? '))
+t = (d * 60) + (km * 0.15)
+print(f'O total a pagar é de R${t:.2f}')
